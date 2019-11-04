e26ea2c067b6db941b92931c98635722266de676
Maria <maria.modan95@gmail.com>
2019-11-04 11:35:30 +0200

    firts commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ItemsContainer from './ItemsContainer'; 
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <ItemsContainer />
+            </div>
+        )
+    }
+}
+
+export default Homepage;
